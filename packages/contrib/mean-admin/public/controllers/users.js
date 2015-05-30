dfb04e5f04be84bd5a0e71255685f3a2d4c538b4
Cesar Tolosa Franco <cesar.tf.cb@gmail.com>
2015-05-30 14:07:50 -0600

    mejoras en el frontend, pantalla de login, creacion de nuevo usuario y botones

--- packages/contrib/mean-admin/public/controllers/users.js
+++ packages/contrib/mean-admin/public/controllers/users.js
@@ -26,11 +26,42 @@ angular.module('mean.mean-admin').controller('UsersController', ['$scope', 'Glob
             inTable: true
         }];
         $scope.user = {};
+        $scope.edit = false;
+
+            $scope.goEdit = function(){
+              console.log('goEdit');
+              $scope.edit = true;
+            };
+
+            $scope.editDone = function(){
+              console.log('editDone');
+              $scope.edit = false;
+            };
 
         $scope.init = function() {
             Users.query({}, function(users) {
                 $scope.users = users;
             });
+        };
+
+        var getMessage = function(response) {
+            var message;
+
+            if (response.data && response.data instanceof Array && response.data[0].msg) {
+                message = response.data[0].msg;
+            } else {
+                message = 'User saved<span style="width: 6px;"></span><span class="yellow-text"> ' + (response.name ? 'Successfully' : 'Failed') + ' </span>';
+            }
+
+            return message;
+        };
+
+        $scope.openModal = function(modalSelector) {
+            $(modalSelector).openModal();
+        };
+        
+        $scope.closeModal = function(modalSelector) {
+            $(modalSelector).closeModal();
         };
 
         $scope.add = function() {
@@ -40,14 +71,20 @@ angular.module('mean.mean-admin').controller('UsersController', ['$scope', 'Glob
                 email: $scope.user.email,
                 name: $scope.user.name,
                 username: $scope.user.username,
-                password: '$scope.user.password',
-                confirmPassword: '$scope.user.password',
-                roles: $scope.user.roles
+                password: $scope.user.password,
+                confirmPassword: $scope.user.password,
+                roles: $scope.user.isAdmin ? ['authenticated', 'admin'] : ['authenticated']
             });
 
             user.$save(function(response) {
                 $scope.user = {};
+                console.log(response);
                 $scope.users.push(response);
+                $scope.closeModal('#new-user-modal');
+                Materialize.toast(getMessage(response), 2000);
+            }, function(error) {
+                console.log(error);
+                Materialize.toast(getMessage(error), 2000);
             });
 
             this.firstName = this.lastName = this.email = this.password = this.role = '';
@@ -79,5 +116,7 @@ angular.module('mean.mean-admin').controller('UsersController', ['$scope', 'Glob
                 user.tmpRoles = user.roles;
             }
         };
+
+        $('.modal-trigger').leanModal();
     }
 ]);
